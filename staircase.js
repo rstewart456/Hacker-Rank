6cf455490464ae94f6e75130fdcd5f9add5f6a7f
rstewart456 <rstewart456@gmail.com>
2019-06-01 17:12:17 -0400

    staircase with spaces and #

---
+++ staircase.js
@@ -0,0 +1,10 @@
+function staircase(n) {
+    var a, b, c;
+    for (let i = 0; i < n; i++) {
+        a = n - 1 -i; //spaces
+        b = i + 1; //hash sign
+        c = " ".repeat(a) + '#'.repeat(b);
+        console.log(c)
+    }
+}
+console.log(staircase(6));
